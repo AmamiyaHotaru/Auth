ad3a833b0ee621d35757c4d87c1693e326230243
AmamiyaHotaru <eunie@eunie.online>
2025-04-28 13:44:30 +0800

    add 谷歌身份验证器令牌导入

--- app.go
+++ app.go
@@ -7,17 +7,18 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/pquerna/otp"
 	"image"
 	_ "image/gif"  // 支持GIF格式
 	_ "image/jpeg" // 支持JPEG格式
 	_ "image/png"  // 支持PNG格式
 	"log"
+	"strings"
 	"time"
 
+	gotp "auth/utils/otp_extractor"
 	"github.com/makiuchi-d/gozxing"
 	"github.com/makiuchi-d/gozxing/qrcode"
-	"github.com/pquerna/otp"
-
 	"github.com/pquerna/otp/totp"
 )
 
@@ -109,13 +110,13 @@ func (a *App) RecognizeQRCode(imgBytes []byte) error {
 	img, format, err := image.Decode(reader)
 	if err != nil {
 		log.Printf("图像解码失败: %v, 格式: %s\n", err, format)
-		return fmt.Errorf("failed to decode image: %w", err)
+		return fmt.Errorf("无法解码图像: %w", err)
 	}
 
 	// prepare BinaryBitmap
 	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
 	if err != nil {
-		return fmt.Errorf("failed to create BinaryBitmap: %w", err)
+		return fmt.Errorf("无法创建 BinaryBitmap: %w", err)
 	}
 
 	// decode image
@@ -123,24 +124,62 @@ func (a *App) RecognizeQRCode(imgBytes []byte) error {
 	result, err := qrReader.Decode(bmp, nil)
 
 	if err != nil {
-		return fmt.Errorf("failed to decode QR code: %w", err)
+		return fmt.Errorf("解析二维码错误: %w,%s", err, "尝试单独截取二维码部分的图片进行解析")
 	}
 
 	fmt.Println(result)
 
-	key, err := otp.NewKeyFromURL(result.GetText())
-	if err != nil {
-		return fmt.Errorf("failed to get otp info: %w", err)
-	}
+	switch {
+	case strings.HasPrefix(result.GetText(), "otpauth-migration://"):
+		// 处理 谷歌认证器 otpauth-migration 前缀
 
-	fmt.Println("Account:", key.AccountName())
-	fmt.Println("Issuer:", key.Issuer())
-	fmt.Println("Secret:", key.Secret())
+		entries, err := gotp.ExtractOtpFromUrl(result.GetText())
+		if err != nil {
+			return fmt.Errorf("解析谷歌验证器导出码失败: %w", err)
+		}
 
-	// 添加解析出的验证码信息
-	err = a.InsertSecret(key.AccountName(), key.Issuer(), key.Secret(), 0)
-	if err != nil {
-		return fmt.Errorf("failed to add account: %w", err)
+		// 打印提取的OTP信息
+		for i, entry := range entries {
+			otpType := 0
+			fmt.Printf("\n%d. 密钥\n", i+1)
+			fmt.Printf("名称:    %s\n", entry.Name)
+			fmt.Printf("密钥:    %s\n", entry.Secret)
+			if entry.Issuer != "" {
+				fmt.Printf("发行者:  %s\n", entry.Issuer)
+			}
+			fmt.Printf("类型:    %s\n", entry.Type)
+			if entry.Type == "hotp" {
+				otpType = 2
+				fmt.Printf("计数器:  %d\n", entry.Counter)
+			}
+
+			err = a.InsertSecret(entry.Name, entry.Issuer, entry.Secret, otpType)
+			if err != nil {
+				return fmt.Errorf("添加账户失败: %w", err)
+			}
+		}
+
+	case strings.HasPrefix(result.GetText(), "otpauth://totp"):
+		// 处理 totp 前缀
+		key, err := otp.NewKeyFromURL(result.GetText())
+		if err != nil {
+			return fmt.Errorf("获取 OTP 信息失败: %w", err)
+		}
+
+		fmt.Println("Account:", key.AccountName())
+		fmt.Println("Issuer:", key.Issuer())
+		fmt.Println("Secret:", key.Secret())
+
+		// 添加解析出的验证码信息
+		err = a.InsertSecret(key.AccountName(), key.Issuer(), key.Secret(), 0)
+		if err != nil {
+			return fmt.Errorf("添加账户失败: %w", err)
+		}
+
+	//case strings.HasPrefix(result.GetText(), "yet-another-prefix://"):
+	//	// 处理 yet-another-prefix 前缀
+	default:
+		return fmt.Errorf("二维码格式错误: %w", err)
 	}
 
 	return nil
